835bc2756d34b39615a36d13c8e60dc5d9bb245e
Tanapat Namsomboon <tanapat.nams@outlook.com>
2025-08-02 18:17:46 +0700

    Add more types for character

--- Alisa/src/Alisa/Core/Types.h
+++ Alisa/src/Alisa/Core/Types.h
@@ -4,6 +4,7 @@
 
 namespace Alisa
 {
+    // Interger types
     using i8  = int8_t;
     using i16 = int16_t;
     using i32 = int32_t;
@@ -14,6 +15,14 @@ namespace Alisa
     using u32 = uint32_t;
     using u64 = uint64_t;
 
+    // Floating-point types
     using f32 = float;
     using f64 = double;
+
+    // Character types for Unicode support
+    using utf8  = char8_t;  // UTF-8 code unit
+    using utf16 = char16_t; // UTF-16 code unit
+    using utf32 = char32_t; // UTF-32 code unit (Unicode code point)
+
+    using wchar = wchar_t;  // Wide character (platform-dependent)
 }
